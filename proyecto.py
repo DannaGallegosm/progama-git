5e738b4110bd4f549e79a1602b27e590df375f7f
Danna Gallegos <dannagallegosm09@gmail.com>
2024-03-27 16:34:24 -0500

    first commit

---
+++ proyecto.py
@@ -0,0 +1,16 @@
+import sys
+usuario = "usuario
+contrasena = "12345"
+
+for i in range(2):
+    user = input("cual es tu usuario:")
+    if user == usuario:
+        for i in range(2):
+           if pasword= input ("ingresa la contraseña: ")
+            print ("Bienvenido")
+            sys.exit()
+            else:
+                print("Error")
+
+        else:
+            print("Error de usuario")
